36fe063693bb76a697526ba87d6382d391773168
LouaiZahran <louai0nasr@gmail.com>
2022-03-01 16:11:34 +0200

    Added documentation
    Generated JavaDocs

--- Lab 1/src/main/java/IO/FileReader.java
+++ Lab 1/src/main/java/IO/FileReader.java
@@ -5,7 +5,16 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * File IO
+ */
 public class FileReader {
+    /**
+     * Reads the content of the specified file
+     * @param fileName The name of the files containing words to be converted to a list
+     * @return The words written in the file as a list
+     * @throws FileNotFoundException if the file is not found
+     */
     public static ArrayList<Object> loadFile(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
