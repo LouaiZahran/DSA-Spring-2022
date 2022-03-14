4c6a5fc7ee9299735c6aadd8d014f101b527af55
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-03-14 19:25:11 +0200

    #8 Analysis fixed for AVL
    changed to all node insertion from each node avg insertion

--- Lab 1/src/src/test/java/Analysis.java
+++ Lab 1/src/src/test/java/Analysis.java
@@ -5,57 +5,61 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class Analysis {
-    String getString(int n){
-        String stringFactory="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    String getString(int n) {
+        String stringFactory = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                 + "0123456789"
                 + "abcdefghijklmnopqrstuvxyz";
-        String ret="";
-        for(int i=0;i<n;i++){
-            int index=(int) (Math.random()*stringFactory.length());
-            ret+=(char)stringFactory.charAt(index);
+        String ret = "";
+        for (int i = 0; i < n; i++) {
+            int index = (int) (Math.random() * stringFactory.length());
+            ret += (char) stringFactory.charAt(index);
         }
         return ret;
     }
+
     @Test
-    public void AnalyseAVL(){
-        long startTime=0;
-        long endTime=0;
+    public void AnalyseAVL() {
+        long startTime = 0;
+        long endTime = 0;
         Tree<String> tree = new AVL<String>();
-        ArrayList<Double> insertionAvgTime=new ArrayList<>();
-        ArrayList<Double> deletionAvgTime=new ArrayList<>();
-        ArrayList<String> arrayToDelete=new ArrayList<>();
-        double timeAvg=0;
-        double sum=0;
-        for(int i=1;i<=1000000;i*=10){
-            for (int j=0;j<i;j++){
-                String str=getString(40);
-                arrayToDelete.add(str);
-                startTime=System.nanoTime();
-                tree.insert(str);
-                endTime=System.nanoTime()-startTime;
-                sum+=(double) endTime;
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        ArrayList<Double> insertionAvgTimeArr = new ArrayList<>();
+        ArrayList<Double> deletionAvgTimeArr = new ArrayList<>();
+
+        double sumDeletion = 0;
+        double sumInsertion = 0;
+
+        for (int i = 10; i <= 1000000; i *= 10) {
+            for (int k = 0; k < 10; k++) {
+                stringArrayList.clear();
+                for (int j = 0; j < i; j++) {
+                    stringArrayList.add(getString(40));
+                }
+                startTime = System.currentTimeMillis();
+                for (int j = 0; j < i; j++) {
+                    tree.insert(stringArrayList.get(j));
+                }
+                endTime = System.currentTimeMillis() - startTime;
+                sumInsertion += (double) endTime;
+
+                startTime = System.currentTimeMillis();
+                for (int j = 0; j < i; j++) {
+                    tree.delete(stringArrayList.get(j));
+                }
+                endTime = System.currentTimeMillis() - startTime;
+                sumDeletion += (double) endTime;
             }
-            insertionAvgTime.add(sum/i);
-            sum=0;
-            for (int j=0;j<i;j++){
-                int index=(int) (Math.random()*arrayToDelete.size())-1;
-                String deletedString= arrayToDelete.remove(index==-1?0:index);
-                startTime=System.nanoTime();
-                tree.delete(deletedString);
-                endTime=System.nanoTime()-startTime;
-                sum+=(double) endTime;
-            }
-            deletionAvgTime.add(sum/i);
-            sum=0;
+            insertionAvgTimeArr.add(sumInsertion/10);
+            deletionAvgTimeArr.add(sumDeletion/10);
         }
         System.out.println("Insertion Avg Time");
-        for(int i=0 , n = 1 ;i<insertionAvgTime.size() && n<=1000000;i++ ,n*=10){
-            System.out.println("at "+"n="+n +" :"+insertionAvgTime.get(i) + "ns");
+        for (int i = 0, n = 10; i < insertionAvgTimeArr.size() && n <= 1000000; i++, n *= 10) {
+            System.out.println("at " + "n=" + n + " :" + insertionAvgTimeArr.get(i) + "ms");
         }
 
         System.out.println("Deletion Avg Time");
-        for(int i=0 , n = 1 ;i<deletionAvgTime.size() && n<=1000000;i++ ,n*=10){
-            System.out.println("at "+"n="+n+" :"+deletionAvgTime.get(i) + "ns");
+        for (int i = 0, n = 10; i < deletionAvgTimeArr.size() && n <= 1000000; i++, n *= 10) {
+            System.out.println("at " + "n=" + n + " :" + deletionAvgTimeArr.get(i) + "ms");
         }
     }
 
